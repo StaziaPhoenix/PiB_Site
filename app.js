6bc9ce7f7e77a9e299e063bcbb62df74e39af0d0
WigginWannabe <stazia@tronboll.us>
2017-08-27 20:17:26 -0700

    mockup of projects page

--- app.js
+++ app.js
@@ -4,6 +4,119 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/assets'));
 
+var projects = [
+					{
+						title: "Binary Clock",
+						page: "binaryclock",
+						mainImage: "/images/clockMain.JPG",
+						images: ["/images/clock1.jpg", "/images/clock2.jpg"],
+						description: "Vivamus sagittis, velit eu accumsan commodo, felis sem lacinia velit, vel blandit ligula nibh sit amet risus."
+					},
+					{
+						title: "Programmable Heart",
+						page: "programmableheart",
+						mainImage: "/images/heartMain.jpg",
+						images: ["/images/heart1.jpg"],
+						description: "In hac habitasse platea dictumst."
+					},
+					{
+						title: "ATTiny Programmer",
+						page: "attinyprogrammer",
+						mainImage: "/images/attinyMain.jpg",
+						images: ["/images/attiny1.jpg"],
+						description: "Suspendisse malesuada congue sodales."
+					},
+					{
+						title: "Binary Clock",
+						mainImage: "/images/clockMain.JPG",
+						images: ["/images/clock1.jpg", "/images/clock2.jpg"],
+						description: "Vivamus sagittis, velit eu accumsan commodo, felis sem lacinia velit, vel blandit ligula nibh sit amet risus."
+					},
+					{
+						title: "Programmable Heart",
+						mainImage: "/images/heartMain.jpg",
+						images: ["/images/heart1.jpg"],
+						description: "In hac habitasse platea dictumst."
+					},
+					{
+						title: "ATTiny Programmer",
+						mainImage: "/images/attinyMain.jpg",
+						images: ["/images/attiny1.jpg"],
+						description: "Suspendisse malesuada congue sodales."
+					},
+					{
+						title: "Binary Clock",
+						mainImage: "/images/clockMain.JPG",
+						images: ["/images/clock1.jpg", "/images/clock2.jpg"],
+						description: "Vivamus sagittis, velit eu accumsan commodo, felis sem lacinia velit, vel blandit ligula nibh sit amet risus."
+					},
+					{
+						title: "Programmable Heart",
+						mainImage: "/images/heartMain.jpg",
+						images: ["/images/heart1.jpg"],
+						description: "In hac habitasse platea dictumst."
+					},
+					{
+						title: "ATTiny Programmer",
+						mainImage: "/images/attinyMain.jpg",
+						images: ["/images/attiny1.jpg"],
+						description: "Suspendisse malesuada congue sodales."
+					},
+					{
+						title: "Binary Clock",
+						mainImage: "/images/clockMain.JPG",
+						images: ["/images/clock1.jpg", "/images/clock2.jpg"],
+						description: "Vivamus sagittis, velit eu accumsan commodo, felis sem lacinia velit, vel blandit ligula nibh sit amet risus."
+					},
+					{
+						title: "Programmable Heart",
+						mainImage: "/images/heartMain.jpg",
+						images: ["/images/heart1.jpg"],
+						description: "In hac habitasse platea dictumst."
+					},
+					{
+						title: "ATTiny Programmer",
+						mainImage: "/images/attinyMain.jpg",
+						images: ["/images/attiny1.jpg"],
+						description: "Suspendisse malesuada congue sodales."
+					},
+					{
+						title: "Binary Clock",
+						mainImage: "/images/clockMain.JPG",
+						images: ["/images/clock1.jpg", "/images/clock2.jpg"],
+						description: "Vivamus sagittis, velit eu accumsan commodo, felis sem lacinia velit, vel blandit ligula nibh sit amet risus."
+					},
+					{
+						title: "Programmable Heart",
+						mainImage: "/images/heartMain.jpg",
+						images: ["/images/heart1.jpg"],
+						description: "In hac habitasse platea dictumst."
+					},
+					{
+						title: "ATTiny Programmer",
+						mainImage: "/images/attinyMain.jpg",
+						images: ["/images/attiny1.jpg"],
+						description: "Suspendisse malesuada congue sodales."
+					},
+					{
+						title: "Binary Clock",
+						mainImage: "/images/clockMain.JPG",
+						images: ["/images/clock1.jpg", "/images/clock2.jpg"],
+						description: "Vivamus sagittis, velit eu accumsan commodo, felis sem lacinia velit, vel blandit ligula nibh sit amet risus."
+					},
+					{
+						title: "Programmable Heart",
+						mainImage: "/images/heartMain.jpg",
+						images: ["/images/heart1.jpg"],
+						description: "In hac habitasse platea dictumst."
+					},
+					{
+						title: "ATTiny Programmer",
+						mainImage: "/images/attinyMain.jpg",
+						images: ["/images/attiny1.jpg"],
+						description: "Suspendisse malesuada congue sodales."
+					}
+				];
 
 /********* ROUTES *********/
 
@@ -12,8 +125,16 @@ app.get("/", function(req, res) {
 });
 
 app.get("/projects", function(req, res) {
-	res.render("projects");
+	res.render("projects", {projects: projects});
+});
+
+app.get("/projects/:page", function(req, res) {
+	res.render("project_details", {project: })
 })
+
+app.get("/getinvolved", function(req, res) {
+	res.render("getinvolved");
+});
 
 /********* END ROUTES *********/
 
